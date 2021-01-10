b7b4945c8fced3768e332169c83a4cf49c3835a1
xuhaodong <xuhaodong@51yund.com>
2021-01-11 00:20:59 +0800

    feat: delete ts

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
 import {
     Menu,
     Submenu,
@@ -11,7 +10,6 @@ import 'element-ui/lib/theme-chalk/index.css'
 import 'font-awesome/css/font-awesome.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'amis/lib/themes/default.css';
-import './style/reset.css';
 
 import Mock from './mock/index';
 Mock.init()
@@ -21,6 +19,5 @@ Vue.use(MenuItem)
 Vue.config.productionTip = false
 new Vue({
     router,
-    store,
     render: h => h(App)
 }).$mount('#app')
--- src/mock/index.js
+++ src/mock/index.js
@@ -2,7 +2,7 @@
 import axios from 'axios'
 import MockAdapter from 'axios-mock-adapter'
 
-function wait(duration: number) {
+function wait(duration) {
     return new Promise((resovle) => {
         setTimeout(resovle, duration);
     });
